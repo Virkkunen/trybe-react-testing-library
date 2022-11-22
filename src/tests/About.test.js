edc2e461088f7edeaaa4337bb52b493e38178cec
Virkkunen <vrkknn@vrkknn.net>
2022-11-22 16:11:00 -0300

    req 02, segunda validacao

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -27,6 +27,8 @@ describe('O componente About.js', () => {
 
     const imgSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const imgEl = screen.getByRole('img');
+
+    expect(imgEl).toBeInTheDocument();
     expect(imgEl.src).toBe(imgSrc);
   });
 });
