2afdf4fce4bca69ab6c062c0d1100edf1187ae0a
Virkkunen <vrkknn@vrkknn.net>
2022-11-22 16:25:57 -0300

    req 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('O componente FavoritePokemon.js', () => {
+  test('exibe na tela a mensagem "No favorite pokémon found" caso não tenha pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const notfoundEl = screen.getByText('No favorite Pokémon found', { exact: true });
+    expect(notfoundEl).toBeInTheDocument();
+  });
+});
